affc5e1ea1d9bdafd1dbaef7ed16a8281a5470d0
Alae Belaizi <albelaiz@c1r8p1.1337.ma>
2024-12-13 19:54:01 +0100

    salina

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -5,18 +5,98 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: albelaiz <albelaiz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/12 13:09:52 by albelaiz          #+#    #+#             */
-/*   Updated: 2024/12/13 10:46:57 by albelaiz         ###   ########.fr       */
+/*   Created: 2024/12/12 13:10:11 by albelaiz          #+#    #+#             */
+/*   Updated: 2024/12/13 19:49:10 by albelaiz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h"
+#include "get_next_line.h"
 
-char *get_next_line(int fd)
+char	*read_and_append(int fd, char *copy)
 {
-	static char  *copy;
-	if (fd < 0 || BUFFER_SEZE <= 0 || raed(fd,0,0) <= 0)
+	char	*buffer;
+	char	*tmp;
+	int		bytes;
+
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	bytes = 1;
+	while (bytes)
 	{
-		return(NULL);
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (bytes <= 0)
+			break ;
+		buffer[bytes] = '\0';
+		if (!copy)
+			copy = ft_strdup("");
+		tmp = copy;
+		copy = ft_strjoin(tmp, buffer);
+		free(tmp);
+		if (!copy)
+			break ;
+		if (ft_strchr(buffer, '\n'))
+			break ;
 	}
+	return (free(buffer), copy);
+}
+
+char	*ft_extract_line(char *str)
+{
+	int		i;
+	char	*line;
+
+	if (!str || !str[0])
+		return (NULL);
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		i++;
+	line = malloc(i + 1);
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (str[i] && str[i] != '\n')
+	{
+		line[i] = str[i];
+		i++;
+	}
+	if (str[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
+
+char	*ft_after_line(char *copy)
+{
+	int		i;
+	char	*remaining;
+
+	i = 0;
+	while (copy[i] && copy[i] != '\n')
+		i++;
+	if (!copy[i])
+	{
+		free(copy);
+		return (NULL);
+	}
+	remaining = ft_strdup(copy + i + 1);
+	free(copy);
+	return (remaining);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*copy[10240];
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) == -1)
+		return (free(copy[fd]), copy[fd] = NULL, NULL);
+	copy[fd] = read_and_append(fd, copy[fd]);
+	if (!copy[fd])
+		return (NULL);
+	line = ft_extract_line(copy[fd]);
+	copy[fd] = ft_after_line(copy[fd]);
+	return (line);
 }
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -5,19 +5,30 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: albelaiz <albelaiz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/12 13:11:12 by albelaiz          #+#    #+#             */
-/*   Updated: 2024/12/13 10:43:46 by albelaiz         ###   ########.fr       */
+/*   Created: 2024/12/13 19:52:43 by albelaiz          #+#    #+#             */
+/*   Updated: 2024/12/13 19:52:56 by albelaiz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
-#ifndef BUFFER_SEZE
-# define BUFFER_SEZE 42
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
-# include <unistd.h>
 # include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_after_line(char *copy);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlen(const char *str);
+char	*ft_extract_line(char *str);
+char	*ft_strchr(const char *str, int c);
+char	*read_and_append(int fd, char *copy);
+void	*free_buffer(int bytes, char *buffer);
+char	*ft_strncpy(char *s1, char *s2, int len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
